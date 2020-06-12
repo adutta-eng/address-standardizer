38d8ec45e55dad207fef88bc86919a6db1292f6f
Aishani Dutta <adutta7@adutta-eng.localdomain>
2020-06-12 16:13:15 -0500

    add basic abbreviation function

--- dictionaries/standardizer.py
+++ dictionaries/standardizer.py
@@ -1,6 +1,35 @@
-from dictionaries.constants import (
+import usaddress
+from constants import (
     DIRECTIONAL_ABBREVIATIONS,
     STATE_ABBREVIATIONS,
     STREET_NAME_ABBREVIATIONS,
     STREET_NAME_POST_ABBREVIATIONS
 )
+
+## Checks if a potential key is contained in a dictionary 
+## Returns corresponding value if so.
+def abbreviate(potential_key, dictionary):
+    if potential_key in dictionary:
+        return dictionary.get(potential_key)
+    else:
+        return potential_key
+
+test_input_a = ">Mi >K Beach Road #2, Kenai, AK 99611"
+print(usaddress.parse(test_input_a))
+
+# USADDRESS
+# AddressNumberPrefix
+# AddressNumber
+# AddressNumberSuffix
+# StreetNamePreModifier
+# StreetNamePreDirectional
+# StreetNamePreType
+# StreetName
+# StreetNamePostType
+# StreetNamePostDirectional
+# SubaddressType
+# SubaddressIdentifier
+# BuildingName?
+
+print(usaddress.parse("1214 Georgetown Way"))
+print(abbreviate('ALASKA', STATE_ABBREVIATIONS))
