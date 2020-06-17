10a6d523b392120f1c51f0da83c23008c0b7173d
Aishani Dutta <adutta7@adutta-eng.localdomain>
2020-06-17 08:07:37 -0500

    add direction and occupancy code conversions

--- dictionaries/standardizer_copy.py
+++ dictionaries/standardizer_copy.py
@@ -6,7 +6,9 @@ from constants import (
     STATE_ABBREVIATIONS,
     STREET_NAME_ABBREVIATIONS,
     STREET_NAME_POST_ABBREVIATIONS,
-    OCCUPANCY_TYPE_ABBREVIATIONS
+    OCCUPANCY_TYPE_ABBREVIATIONS,
+    OCCUPANCY_TYPE_CODES,
+    DIRECTION_CODES
 )
 
 # A mapping between each usaddress category to its standardizing dictionary
@@ -73,6 +75,19 @@ def standardize(unparsed_address):
                 # We standardize by converting to the preferred abbreviation
                 if key == word:
                     parsed_address[index] = update_tuple(address_category_mappings.get(key), category_pair)
+    
+    # Add direction codes and occupancy codes by appending another tuple to parsed_address when a match occurs
+    for (word, label) in parsed_address:
+        if label == "StreetNamePostDirectional":
+            post_directional_code = (DIRECTION_CODES.get(word), "PostDirectionalCode")
+            parsed_address.append(post_directional_code)
+        if label == "StreetNamePreDirectional":
+            pre_directional_code = (DIRECTION_CODES.get(word), "PreDirectionalCode")
+            parsed_address.append(pre_directional_code)
+        if label == "StreetNamePostType":
+            if word in OCCUPANCY_TYPE_CODES.keys():
+                parsed_address.append((OCCUPANCY_TYPE_CODES.get(word), "OccupancyTypeCode"))
+
     return(parsed_address)
 
 print(standardize("Homer Spit Road, Homer, Arkansas 99603"))
@@ -85,3 +100,31 @@ print(standardize("168 Hillside Avenue, Hartford, CT 06106"))
 print(standardize("1025 Vermont Avenue Northwest, Washington, DC 20005"))
 print(standardize("697 North Dupont Boulevard, Milford, DE 19963"))
 print(standardize("1915 North Republic De Cuba Avenue, Tampa, FL 33602"))
+print(standardize("2406 North Slappey Boulevard, Albany, GA 31701"))
+print(standardize("98-1247 Kaahumanu, Aiea, HI 96701"))
+print(standardize("103 West Main, Ute, IA 51060"))
+print(standardize("335 Deinhard Lane, Mc Call, ID 83638"))
+print(standardize("8922 South 1/2 Greenwood Avenue, Chicago, IL 60619"))
+print(standardize("239 West Monroe Street, Decatur, IN 46733"))
+print(standardize("827 Frontage Road, Agra, KS 67621"))
+print(standardize("508 West 6th Street, Lexington, KY 40508"))
+print(standardize("5103 Hollywood Avenue, Shreveport, LA 71109"))
+print(standardize("79 Power Road, Westford, MA 01886"))
+print(standardize("5105 Berwyn Road, College Park, MD 20740"))
+print(standardize("47 Broad Street, Auburn, ME 04210"))
+print(standardize("470 South Street, Ortonville, MI 48462"))
+print(standardize("404 Wilson Avenue, Faribault, MN 55021"))
+print(standardize("5933 Mc Donnell Boulevard, Hazelwood, MO 63042"))
+print(standardize("918 East Main Avenue, Lumberton, MS 39455"))
+print(standardize("107 A Street East, Poplar, MT 59255"))
+print(standardize("Village Shps Of Bnr, Banner Elk, NC 28604"))
+print(standardize("2601 State Street, Bismarck, ND 58501"))
+print(standardize("207 South Bell Street, Fremont, NE 68025"))
+print(standardize("107 State Street, Portsmouth, NH 03801"))
+print(standardize("1413 State Highway #50, Mays Landing, NJ 08330"))
+print(standardize("I-25 Highway 87, Raton, NM 87740"))
+print(standardize("516 West Goldfield Avenue, Yerington, NV 89447"))
+print(standardize("2787 Bway Way, New York, NY 10001"))
+print(standardize("1380 Bethel Road, Columbus, OH 43220"))
+print(standardize("305 Main, Fort Cobb, OK 73038"))
+print(standardize("17375 Southwest Tualatin Valley Hwy, Beaverton, OR 97006"))
