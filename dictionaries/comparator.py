c912b1fc280ea5a301cb78d36aaf49ee1879cf7d
evan-dong <61509624+evan-dong@users.noreply.github.com>
2020-07-16 17:11:06 -0500

    reworked to guarante order when joining labels
    dict.update is not guaranteed to be ordered, but constructing the dict on an extended list does

--- dictionaries/comparator.py
+++ dictionaries/comparator.py
@@ -13,23 +13,17 @@ ouput: the relevant values of address_dict, in appropriate order
 Dict[label, word/code] -> List[word/code]
 """
 def fid_prepare(address_dict):
-    # as per SNSDC: ["OSN", "ZIP", "SNPTC", "SNSTC", "SNEC", "SNPDC", "SNSDC"]
     # the labels in order, with their appropriate default/null values
-    fid_order = OrderedDict([("OSN", ''), ("ZIP", 0), ("SNPTC", 0), ("SNSTC", 0), \
-        ("SNEC", 0), ("SNPDC", 0), ("SNSDC", 0)])
-    
-    # # could theoretically get rid of the list & use an ordered dict but that's a lot of work
-    # fid_defaults = {"OSN", '', "ZIP", 0, "SNPTC", 0, "SNSTC", 0, "SNEC", 0, "SNPDC", 0, "SNSDC", 0}
-    # HN_order = ["HN1", "HNSEP", "HN2"]
-    HN_order =OrderedDict([("HN1", 0), ("HNSEP", ""), ("HN2", 0)])
-    WS_order = OrderedDict([("WSDESC1", 0), ("WSID1", 0)])
-    #["StreetName", "Zip", "SteetPreTypeCode", "SteetPostTypeCode", "SNEC", "PreDirectionalCode", "PostDirectionalCode"]
-    # figure out how this works in dataframes/NaN/nulls? .isnull?
+    fid_order = [("OSN", ''), ("ZIP", 0), ("SNPTC", 0), ("SNSTC", 0), \
+        ("SNEC", 0), ("SNPDC", 0), ("SNSDC", 0)]
+    HN_order = [("HN1", 0), ("HNSEP", ""), ("HN2", 0)]
+    WS_order = [("WSDESC1", 0), ("WSID1", 0)]
+
+    # compose a this dict.
+
+    comparison_labels = OrderedDict(fid_order)
     result = [address_dict[label] if label in address_dict else \
-        fid_order[label] for label in fid_order.keys()]
-    # result[0] = str(result[0])
-    # result[1] = str(result[1])
-    #is this the appropriate way to handle this?
+        comparison_labels[label] for label in comparison_labels.keys()]
     return result
 
 def fid_pair(in_list, match_list):
