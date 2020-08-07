02581bb2b77d5fe74ad962a0740ee4cd47c7280c
evan-dong <61509624+evan-dong@users.noreply.github.com>
2020-08-07 16:21:26 -0500

    cleaned up struture - no more orderedDict

--- tools/matching/comparator.py
+++ tools/matching/comparator.py
@@ -1,61 +1,65 @@
 from ..standardization.standardizer import standardize
-from collections import OrderedDict
-# from . import amgScore
 
 # fidCompare: Name, Zip, PreType, SufType, ExtType, PreDir, SufDir
 # -> StreetName, Zip, StreetNamePreType, StreetNamePostType, ??? StreetNamePreDirectional, StreetNamePostDirectional
 # function: strip needed values; find defaults from amgScore.py
-fid_order = [("OSN", ''), ("ZIP", 0), ("SNPTC", 0), ("SNSTC", 0), \
-    ("SNEC", 0), ("SNPDC", 0), ("SNSDC", 0)]
-HN_order = [('HN', 0), ("HN1", 0), ("HNSEP", ""), ("HN2", 0), ('HNPRE', ""), ('HNSUF', "")]
-WS_order = [("WSDESC1", 0), ("WSID1", 0)]
 
-exact_matches =  [('HN', 0), ('HNPRE', ""), ('HNSUF', ""), ('WS', ""), \
-    ('OccupancyType', ""), ('OccupancyIdentifier', 0)]
-exact_match_labels =  ['HN', 'HNPRE', 'HNSUF', 'WS', 'OccupancyType', 'OccupancyIdentifier']
 
-label_shortcuts = {'fid' : fid_order, 'HN' : HN_order, 'WS' : \
-    WS_order, 'exact' : exact_matches}
+exact_matches = ['HN', 'HNPRE', 'HNSUF', 'WS', 'OccupancyType', 'OccupancyIdentifier']
+fid_order = ['OSN', 'ZIP', 'SNPTC', 'SNSTC', 'SNEC', 'SNPDC', 'SNSDC']
+HN_order = ['HN', 'HN1', 'HNSEP', 'HN2', 'HNPRE', 'HNSUF']
+WS_order = ["WSDESC1", "WSID1"]
 
+label_shortcuts = {'fid_order' : fid_order, 'HN_order' : HN_order, 'WS_order' : \
+    WS_order, 'exact_matches' : exact_matches}
+
+all_labels = {
+    'OSN': '', 
+    'ZIP': 0, 
+    'SNPTC': 0, 
+    'SNSTC': 0, 
+    'SNEC': 0, 
+    'SNPDC': 0, 
+    'SNSDC': 0, 
+    'HN': 0, 
+    'HN1': 0, 
+    'HNSEP': '', 
+    'HN2': 0, 
+    'HNPRE': '', 
+    'HNSUF': '', 
+    'WSDESC1': 0, 
+    'WSID1': 0, 
+    'WS': '', 
+    'OccupancyType': '', 
+    'OccupancyIdentifier': 0
+}
 
 """
 makes a list corresponding to the arguments to fidCompare, with zero if null
 input: address_dict, the output of standardize in dict form
        labels, specific values for the function to pull from the dict
-##### dict of comparison values?
-ouput: the relevant values of address_dict, in appropriate order, substituted appropriately if absent
+       substitutions, the default value if the label isn't in address_dict 
+ouput: the relevant values of address_dict, in order, substituted if absent
 Dict[label, word/code] -> List[word/code]
 """
-# def fid_prepare(address_dict):
-#     # the labels in order, with their appropriate default/null values
-#     fid_order = [("OSN", ''), ("ZIP", 0), ("SNPTC", 0), ("SNSTC", 0), \
-#         ("SNEC", 0), ("SNPDC", 0), ("SNSDC", 0)]
-#     HN_order = [("HN1", 0), ("HNSEP", ""), ("HN2", 0)]
-#     WS_order = [("WSDESC1", 0), ("WSID1", 0)]
+def pull_labels(address_dict, labels = fid_order, substitutions = None):
+        
+    # add in any extra substitutions
+    if substitutions is not None:
+        master_substitutions = {**all_labels, **dict(substitutions)}
+    else:
+        master_substitutions = all_labels
 
-#     # compose a this dict.
-
-
-#     comparison_labels = OrderedDict(fid_order)
-#     result = [address_dict[label] if label in address_dict else \
-#         comparison_labels[label] for label in comparison_labels.keys()]
-#     return result
-
-
-def pull_labels(address_dict, labels = ['fid']):
-    # the labels in order, with their appropriate default/null values
-    full_order = []
+    # add any quick label packages
+    comparison_labels = []
     for label in labels:
         if label in label_shortcuts:
-            full_order.extend(label_shortcuts[label])
+            comparison_labels.extend(label_shortcuts[label])
         else:
-            full_order.append(label)
-    # full_order = [label  for label in labels if label not in label_shortcuts else l for l in label_shortcuts[label]]
-    # compose a full dict
-    comparison_labels = OrderedDict(full_order)
+            comparison_labels.append(label)     
     
     result = [address_dict[label] if label in address_dict else \
-        comparison_labels[label] for label in comparison_labels.keys()]
+        master_substitutions[label] for label in comparison_labels]
     return result
 
 
